d45113f04cd52e0273f93ea602e589e962f415ad
BabaJagaWiedzma <sobieszek.@gmail.com>
2021-03-20 23:38:14 +0100

    loki

--- wiedzma/src/wiedzma/HelloWorld.java
+++ wiedzma/src/wiedzma/HelloWorld.java
@@ -3,8 +3,9 @@ package wiedzma;
 public class HelloWorld {
 	public static void main(String[] args) {
 
-		if (System.out.printf("Namaste duniya") == null) {
+		if (System.out.printf("\\\\n \"BABA JAGA\"") == null) {
 			
 		}
 }
+	
 	}
---
+++ wiedzma/src/wiedzma/NumberServiceProxy.java
@@ -0,0 +1,33 @@
+package wiedzma;
+
+public class NumberServiceProxy {
+
+	public static void main(String[] args) 		{
+
+//	        char lokesh = 'd';
+//	        int ania = lokesh;
+//	       
+//	        int wiedzma = (int) ania;
+//
+//	        for (int i=ania/ania ; i<=(wiedzma); i++) {
+//	        	
+//	        	System.out.println(i);	
+//	        }
+//		int one= 'A'/'A';
+//		for(int x = one; x <= 'd'; x++)
+//		{
+//		System.out.println(x);
+//		}
+		
+		int one= 'A'/'A';
+		String s= "anialokesh";
+		int hundred = s.length()*s.length();
+		for(int x = one; x <= hundred; x++)
+		{
+		System.out.println(x);
+		}
+		
+	    }
+	
+
+	}
